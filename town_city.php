395901cb53f78a0c95e223667d35c98943e96601
Zoe Buesa <141889450+buesazoe@users.noreply.github.com>
2023-11-28 06:47:17 +0800

    Update town_city.php

--- town_city.php
+++ town_city.php
@@ -1,5 +1,5 @@
 <?php
-include_once("db.php"); // Include the Database class file
+include_once("db.php"); // Include the file with the Database class
 
 class TownCity {
     private $db;
@@ -8,17 +8,119 @@ class TownCity {
         $this->db = $db;
     }
 
-    public function getAll() {
+    public function create($data) {
         try {
-            $sql = "SELECT * FROM town_city";
+            // Prepare the SQL INSERT statement
+            $sql = "INSERT INTO town_city(name) VALUES(:name);";
             $stmt = $this->db->getConnection()->prepare($sql);
+
+            // Bind values to placeholders
+            $stmt->bindParam(':name', $data['name']);
+
+            // Execute the INSERT query
             $stmt->execute();
 
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            // Check if the insert was successful
+             
+            if($stmt->rowCount() > 0)
+            {
+                return $this->db->getConnection()->lastInsertId();
+            }
+
         } catch (PDOException $e) {
-            // Handle errors (log or display)
+            // Handle any potential errors here
+            echo "Error: " . $e->getMessage();
             throw $e; // Re-throw the exception for higher-level handling
         }
     }
+
+    public function read($id) {
+        try {
+            $connection = $this->db->getConnection();
+
+            $sql = "SELECT * FROM town_city WHERE id = :id";
+            $stmt = $connection->prepare($sql);
+            $stmt->bindValue(':id', $id);
+            $stmt->execute();
+
+            // Fetch the student data as an associative array
+            $townCity = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            return $townCity;
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            throw $e; // Re-throw the exception for higher-level handling
+        }
+    }
+
+    public function update($id, $data) {
+        try {
+            $sql = "UPDATE town_city SET
+                    name = :name
+                    WHERE id = :id";
+
+            $stmt = $this->db->getConnection()->prepare($sql);
+            // Bind parameters
+            $stmt->bindValue(':id', $data['id']);
+            $stmt->bindParam(':name', $data['name']);
+
+            // Execute the query
+            $stmt->execute();
+
+            return $stmt->rowCount() > 0;
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            throw $e; // Re-throw the exception for higher-level handling
+        }
+    }
+
+    public function delete($id) {
+        try {
+            $sql = "DELETE FROM town_city WHERE id = :id";
+            $stmt = $this->db->getConnection()->prepare($sql);
+            $stmt->bindValue(':id', $id);
+            $stmt->execute();
+
+            // Check if any rows were affected (record deleted)
+            if ($stmt->rowCount() > 0) {
+                return true; // Record deleted successfully
+            } else {
+                return false; // No records were deleted (student_id not found)
+            }
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            throw $e; // Re-throw the exception for higher-level handling
+        }
+    }
+
+    public function displayAll(){
+        try {
+            $sql = "SELECT * FROM town_city LIMIT 10"; // Modify the table name to match your database
+            $stmt = $this->db->getConnection()->prepare($sql);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        } catch (PDOException $e) {
+            // Handle any potential errors here
+            echo "Error: " . $e->getMessage();
+            throw $e; // Re-throw the exception for higher-level handling
+        }
+    }
+
+    public function getAll() {
+        try {
+            $sql = "SELECT * FROM town_city"; // Modify the table name to match your database
+            $stmt = $this->db->getConnection()->prepare($sql);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        } catch (PDOException $e) {
+            // Handle any potential errors here
+            echo "Error: " . $e->getMessage();
+            throw $e;
+        }
+    }
 }
+
+
 ?>
